f3decf317281cbfc64487329450c4f7c4627ea49
ovie009 <ovieiffieu2much@gmail.com>
2023-03-16 20:06:50 +0100

    unset object

--- includes/newclass.inc.php
+++ includes/newclass.inc.php
@@ -3,11 +3,25 @@
     class NewClass {
         
         // properties and methods goes here
-        public $info = "This is a property";
+        private $data = "This is a property";
+
+        public function __construct() {
+            echo  "This class has been instantiated <br>";
+        }
+
+        public function setData($newData) {
+            $this->data = $newData;
+        }
+
+        public function getData() {
+            return $this->data;
+        }
+
+        public function __destruct()
+        {
+            echo "<br> This is the end of the class!";
+        }
+
     }
-
-    $object = new NewCLass;
-
-    var_dump($object);
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     include "./includes/person.inc.php";
+    include "./includes/newclass.inc.php";
 ?>
 
 <!DOCTYPE html>
@@ -17,9 +18,11 @@
         Lorem ipsum dolor sit amet consectetur adipisicing elit. In incidunt inventore commodi a tenetur consequuntur aspernatur repellat molestias temporibus velit?
     </p>
     <strong> <?php
-    
-        $person1 = new Person('John Jones', 'Black', 24);
-        echo $person1->getName();
+
+        $newClass = new NewClass;
+        unset($newClass); // delete instantiated object
+        echo $newClass->getData(); // now this line of code would return an error message
+        // because the class has been removed
 
     ?> </strong>
 
