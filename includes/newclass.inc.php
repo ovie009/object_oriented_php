42c51796ceaf3cf60b87968956d36425734c2609
ovie009 <ovieiffieu2much@gmail.com>
2023-03-16 18:26:12 +0100

    created first object with a public property

---
+++ includes/newclass.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+    class NewClass {
+        
+        // properties and methods goes here
+        public $info = "This is a property";
+    }
+
+    $object = new NewCLass;
+
+    var_dump($object);
+
+?>
